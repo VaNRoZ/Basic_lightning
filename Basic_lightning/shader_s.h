4e86d4e2cc88a0b8c3c0bfc62a170327c54f1a8a
VaNRoZ <giga1345@gmail.com>
2021-07-07 19:39:44 +0300

    Firest try

--- Basic_lightning/shader_s.h
+++ Basic_lightning/shader_s.h
@@ -112,7 +112,7 @@ public:
 	{
 		glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
 	}
-	//-------------------------------
+	//==================================
 	void setInt(const std::string& name, int velue) const
 	{
 		glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
@@ -122,7 +122,7 @@ public:
 	{
 		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 	}
-	//--------------------------------
+	//====================================
 	void setVec2(const std::string& name, const glm::vec2& value) const
 	{
 		glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
@@ -132,5 +132,14 @@ public:
 		glUnifotm2f(glGetUniformLocation(ID, name.c_str()), x, y);
 	}
 	//---------------------------------
+	void setVec3(const std::string& name, const glm::vec3& value) const
+	{
+		glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+	}
+	void setVec3(std::string& name, float x, float y, float z) const
+	{
+		glUniform3f(glGetUniformLoacation(id, name.c_str()), x, y, z);
 
+	}
+	//=====================================
 };
